65c0dc3c7c5cb5975d99ea95cda12f6f8a2433b7
Pranav Verma <pranavverma@Pranavs-MacBook-Air.local>
2026-06-30 13:40:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
     try {
       setLoading(true);
 
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("http://35.254.254.35:8000/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
